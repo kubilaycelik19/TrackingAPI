1d5142d69fed4b4956a0bbce36716ef1f9d22427
kubilay Celik <celikkubilay4006@gmail.com>
2025-12-13 19:29:33 +0300

    CI pipeline eklendi. env dosyası ile secret key soyutlanacak.

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,102 +1,60 @@
 from django.test import TestCase
-from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Expense, Category
 from rest_framework.test import APIClient
 from rest_framework import status
-from django.contrib.auth.models import User
-from .models import Category, Expense
-from . services import ExpenseAnalytics # service class'ı
-# loglama importu
-from logging import getLogger
 
+from logging import getLogger
 logger = getLogger(__name__)
 
-class ExpenseAnalyticsTest(TestCase):
+# ----- Smoke Testi -----
 
-    """
-    ExpenseAnalytics service katmanının test sınıfı
-    """
+# Temel Django modellerinin ve Viewlarının çalışıp çalışmadığı testi. (CI/CD için)
+
+class ExpenseModelTest(TestCase):
     def setUp(self):
-        """
-        Her test çalışmadan önce çalışır ve hazırlık yapar.
-        Gerçek veritabanını bozmaz, test bitince buradaki veriler silinir.
-        """
-
-    # Test kullanıcı oluşturma
-        self.user = User.objects.create_user(username='testuser1', password='testpass123')
-        logger.info(f"Test kullanıcısı oluşturuldu: {self.user.username}")
-
-    # Kategori oluşturma
-        self.cat_food = Category.objects.create(name='Gıda')
-        logger.info(f"Kategori oluşturuldu: {self.cat_food.name}")
-        self.cat_transport = Category.objects.create(name='Ulaşım')
-        logger.info(f"Kategori oluşturuldu: {self.cat_transport.name}")
-
-    # Harcama ekleme
-        Expense.objects.create(user=self.user, amount=800, category=self.cat_food, date='2024-01-15', description='Market alışverişi')
-        logger.info(f"Harcama oluşturuldu: 800 TL, Kategori: {self.cat_food.name}")
-        Expense.objects.create(user=self.user, amount=150, category=self.cat_transport, date='2024-01-20', description='Otobüs bileti')
-        logger.info(f"Harcama oluşturuldu: 150 TL, Kategori: {self.cat_transport.name}")
-
-    def test_calculate_stats(self):
-        """
-        Service katmanı istatistik hesaplama fonksiyonunun testi
-        """
-        analytics = ExpenseAnalytics(self.user) # Service class'ından örnek oluşturma
-        logger.info("ExpenseAnalytics örneği oluşturuldu.")
-        filtered_queryset = analytics.get_filtered_expense({}) # Tüm harcamalar. Boş filtre
-        logger.info(f"Filtrelenmiş harcama queryseti alındı: {filtered_queryset.count()} kayıt bulundu.")
-        stats = analytics.calculate_stats(filtered_queryset) # İstatistik hesaplama fonksiyonu
-        logger.info(f"Harcama istatistikleri hesaplandı: {stats}")
-
-        # Doğrulama aşaması (Assertions)
-
-        # Toplam tutar 950 olmalı (800 + 150)
-        self.assertEqual(stats['total_amount'], 950) # Toplam harcama
-        logger.info(f"Toplam harcama doğrulandı: {stats['total_amount']}")
-
-        # En yüksek harcama kategorisi 'Gıda' olmalı
-        self.assertEqual(stats['most_expensive_category'], 'Gıda') # En yüksek harcama kategorisi
-        logger.info(f"En yüksek harcama kategorisi doğrulandı: {stats['most_expensive_category']}")
         
-        # Gıda harcaması 800, Ulaşım harcaması 150 olmalı
-        self.assertEqual(stats['category_breakdown']['Gıda'], 800)
-        self.assertEqual(stats['category_breakdown']['Ulaşım'], 150)
-        logger.info(f"Kategori bazlı harcama doğrulandı: {stats['category_breakdown']}")
+        self.user = User.objects.create_user(username='testuser', password="123")
+        self.category = Category.objects.create(name="Market")
 
-class ExpenseAPITest(TestCase):
-    """
-    Expense API uç noktasının test sınıfı. TestCase kullanarak API testi yapar.
-    örn: 'APIClient' ile istek gönderme ve yanıt doğrulama, urls yanlış yönlendiriyorsa veya IsAuthenticated izinleri doğru çalıştırıyor mu gibi.
-    APIClient kullanarak kod üzerinden sisteme tarayıcı veya postman vs. gibi istek atar.
-    """
-    def setUp(self):
-        # Sanal tarayıcı oluşturma
-        self.client = APIClient() # APIClient örneği oluşturma
-        logger.info("APIClient örneği oluşturuldu.")
+    def test_create_expense(self):
+        # Model seviyesinde kayıt yapılabiliyor mu testi.
 
-        self.user = User.objects.create_user(username='testuser2', password='testpass456')
-        logger.info(f"Test kullanıcısı oluşturuldu: {self.user.username}")
-        self.url = '/expenses/stats/' # Test edilecek API uç noktası
-        logger.info(f"Test edilecek URL ayarlandı: {self.url}")
+        expense = Expense.objects.create(
+            user=self.user,
+            category=self.category,
+            amount=100.50,
+            description="Test Harcaması",
+            date="2025-01-01"
+        )
+        self.assertEqual(expense.amount, 100.50) # Amount değerinin 100.50 olup olmadığını sorgular
+        self.assertEqual(str(expense), "testuser - Market - 100.5 ₺") # Dönen dict yapısını sorgular.
 
-    def test_unauthenticated_access(self):
-        """
-        Senaryo: Giriş yapmamış kullanıcı API'ye erişmeye çalışıyor.
-        Beklenen Sonuç: 403 Forbidden
-        """
-        # Login yapmadan istek atma işlemi
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        logger.info(f"Yetkilendirilmemiş erişim testi yapıldı, durum kodu: {response.status_code}")
+    class ExpenseAPITest(TestCase):
+        def setUp(self):
+            self.client = APIClient()
+            self.user = User.objects.create_user(username='apiuser', password="123")
+            self.client.force_authenticate(user=self.user) # Login olmuş gibi yap
+            self.category = Category.objects.create(name='Fatura')
 
-    def test_authenticated_access(self):
-        """
-        Senaryo: Giriş yapmış kullanıcı API'ye erişmeye çalışıyor
-        Beklenen Sonuç: 200 OK
-        """
+        def test_get_expenses(self):
+            # Harcama oluştur
+            Expense.objects.create(
+                user = self.user,
+                category = self.category,
+                description = "Api test harcaması",
+                amount = 50,
+                date = "2025-01-01"
+            )
 
-        self.client.force_authenticate(user = self.user) # Kullanıcıyı oturum açmış gibi ayarla
-        logger.info(f"Kullanıcı oturum açtı: {self.user.username}")
-        response = self.client.get(self.url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK) # Başarılı erişim doğrulaması (200 OK)
-        logger.info(f"Yetkilendirilmiş erişim testi yapıldı, durum kodu: {response.status_code}")
+            # Api'den listeyi get ile alma testi
+            response =  self.client.get('/expenses/')
+
+            # Response kontrol
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+            # Listede eleman var mı 
+            self.assertEqual(len(response.data), 1)
+
+
+
